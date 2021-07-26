d703b9fa7df36a1c1a7bb14cab965efa57a7ebd0
pulipativedanth <80522589+pulipativedanth@users.noreply.github.com>
2021-07-26 22:08:13 +0530

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,1 +1,1 @@
-INSERT INTO dbo.[commitdetails ](Repository,CommittedID,Branch,CommitDate,CommitedAuthor) values($(ColValue1),$(ColValue3),$(ColValue2),$(ColValue4),$(ColValue5))
+INSERT INTO [dbo].[commitdetails](Repository,CommittedID,Branch,CommitDate,CommitedAuthor) values($(ColValue1),$(ColValue3),$(ColValue2),$(ColValue4),$(ColValue5))
