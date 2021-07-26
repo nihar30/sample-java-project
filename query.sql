a911d6b338bb6134030b3017937e0cc33ee6ed8d
pulipativedanth <80522589+pulipativedanth@users.noreply.github.com>
2021-07-26 11:55:01 +0530

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,3 +1,72 @@
+
+select 
+	cast(bulkcolumn as nvarchar(max)) as jsondata 
+from 
+	openrowset(bulk 'import-demo/user1.json', data_source = 'dmstore2_misc', single_clob) as azureblob
+go
+
+-- Read and access the content of the JSON file
+with cte as 
+(
+	select 
+		cast(bulkcolumn as nvarchar(max)) as jsondata 
+	from 
+		openrowset(bulk 'import-demo/user1.json', data_source = 'dmstore2_misc', single_clob) as azureblob
+)
+select
+	j.*
+from
+	cte
+cross apply 
+	openjson(cte.jsondata) j
+go
+
+-- Read and access the content of the JSON file, with schema-on-read
+with cte as 
+(
+	select 
+		cast(bulkcolumn as nvarchar(max)) as jsondata 
+	from 
+		openrowset(bulk 'import-demo/user1.json', data_source = 'dmstore2_misc', single_clob) as azureblob
+)
+select
+	j.*
+from
+	cte
+cross apply 
+	openjson(cte.jsondata) with
+	(
+		firstName nvarchar(50),
+		lastName nvarchar(50),
+		isAlive bit,
+		age int,
+		[address] nvarchar(max) as json,
+		phoneNumbers nvarchar(max) as json
+	) j
+go
+
+-- What if source is a list of json rows?
+with cte as
+(
+select 
+	cast(bulkcolumn as nvarchar(max)) as jsondata 
+from 
+	openrowset(bulk 'import-demo/users.json', data_source = 'dmstore2_misc', single_clob) as azureblob
+)
+select
+	s.[value] as jsonrow
+from
+	cte c
+cross apply
+	string_split(c.jsondata, char(10)) s
+where
+	replace(s.[value], char(13), '') <> ''
+go
+
+/*
+	--- CSV / Flat Files ---
+*/
+-- Create target table
 drop table if exists [dbo].[customer];
 create table [dbo].[customer]
 (
@@ -10,4 +79,33 @@ create table [dbo].[customer]
 	[C_MKTSEGMENT] [char](10) not null,
 	[C_COMMENT] [varchar](117) not null
 )
+go
+
+-- Import data
+-- Data types will be inferred from table schema
+bulk insert dbo.[customer]
+from 'import-demo/customer.tbl' 
+with (
+	data_source = 'dmstore2_misc',
+	codepage = '65001',
+	fieldterminator = '|',
+	rowterminator = '|\n'
+)
+go
+
+-- Check imported data
+select top (100) * from [dbo].[customer]
+go
+
+-- Query data without importing
+-- A formatfile is needed to provide required schema
+select 
+	*
+from 
+	openrowset(bulk 'import-demo/customer.tbl', 
+		data_source = 'dmstore2_misc', 
+		codepage = '65001',
+		formatfile = 'import-demo/customer.fmt', 
+		formatfile_data_source = 'dmstore2_misc'
+	) as azureblob
 go
