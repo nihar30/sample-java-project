c9dd51dd231dedac6c0af56f82d7d440be834dff
pulipativedanth <80522589+pulipativedanth@users.noreply.github.com>
2021-07-26 14:51:31 +0530

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,6 +1,6 @@
 INSERT INTO customer (id, name, surname, age, dateOfBirth)
  SELECT id, firstNAme, lastName, age, dateOfBirth 
- FROM OPENJSON(BLUCK './user1.json',SINGLE_BLOB)
+ FROM OPENJSON(BULK './user1.json',SINGLE_BLOB)
  WITH (id int,
        firstName nvarchar(50), lastName nvarchar(50), 
        age int, dateOfBirth datetime2)
