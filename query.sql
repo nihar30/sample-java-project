27cb5c7900521d25260f8474401fdd43c5fe2e76
pulipativedanth <80522589+pulipativedanth@users.noreply.github.com>
2021-07-26 11:27:18 +0530

    Update query.sql
    Creating Table

--- query.sql
+++ query.sql
@@ -1,1 +1,13 @@
-INSERT INTO dbo.ErrorLog VALUES (07/21/2021,'NT',8,8,8,'test',8,'test')
+drop table if exists [dbo].[customer];
+create table [dbo].[customer]
+(
+	[C_CUSTKEY] [int] not null,
+	[C_NAME] [varchar](25) not null,
+	[C_ADDRESS] [varchar](40) not null,
+	[C_NATIONKEY] [int] not null,
+	[C_PHONE] [char](15) not null,
+	[C_ACCTBAL] [decimal](15, 2) not null,
+	[C_MKTSEGMENT] [char](10) not null,
+	[C_COMMENT] [varchar](117) not null
+)
+go
