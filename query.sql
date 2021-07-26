0387595629de91c9c3732cfa82cc61d55f65ce8c
pulipativedanth <80522589+pulipativedanth@users.noreply.github.com>
2021-07-26 13:11:48 +0530

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,64 +1,6 @@
-select 
-	cast(bulkcolumn as nvarchar(max)) as jsondata 
+select  [dbo].[customer]  nvarchar(max)
+	 
 from 
-	openrowset(bulk './user1.json', single_clob) as azureblob
-go
-
--- Read and access the content of the JSON file
-with cte as 
-(
-	select 
-		cast(bulkcolumn as nvarchar(max)) as jsondata 
-	from 
-		openrowset(bulk './user1.json', single_clob) as azureblob
-)
-select
-	j.*
-from
-	cte
-cross apply 
-	openjson(cte.jsondata) j
-go
-
--- Read and access the content of the JSON file, with schema-on-read
-with cte as 
-(
-	select 
-		cast(bulkcolumn as nvarchar(max)) as jsondata 
-	from 
-		openrowset(bulk './user1.json', single_clob) as azureblob
-)
-select
-	j.*
-from
-	cte
-cross apply 
-	openjson(cte.jsondata) with
-	(
-		firstName nvarchar(50),
-		lastName nvarchar(50),
-		isAlive bit,
-		age int,
-		[address] nvarchar(max) as json,
-		phoneNumbers nvarchar(max) as json
-	) j
-go
-
--- What if source is a list of json rows?
-with cte as
-(
-select 
-	cast(bulkcolumn as nvarchar(max)) as jsondata 
-from 
-	openrowset(bulk './users.json',  single_clob) as azureblob
-)
-select
-	s.[value] as jsonrow
-from
-	cte c
-cross apply
-	string_split(c.jsondata, char(10)) s
-where
-	replace(s.[value], char(13), '') <> ''
+	openrowset(bulk './user1.json', single_clob) JSON
 go
 
