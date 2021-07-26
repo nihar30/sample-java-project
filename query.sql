dc0a0735bbdf7cc5cf01d6b132114d40a18c175c
pulipativedanth <80522589+pulipativedanth@users.noreply.github.com>
2021-07-26 14:34:44 +0530

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,4 +1,6 @@
-SELECT *
-FROM OPENROWSET(BULK'./user1.json',SINGLE_BLOB) JSON
-WITH (id int '$.id', firstName nvarchar(50) '$.firstName', lastName nvarchar(50) '$.lastName',
-           age int '$.age', dateOfBirth datetime2 '$.dateOfBirth')
+INSERT INTO customer (id, name, surname, age, dateOfBirth)
+ SELECT id, firstNAme, lastName, age, dateOfBirth 
+ FROM OPENJSON(BLUCK './user1.json',SINGLE_BLOB)
+ WITH (id int,
+       firstName nvarchar(50), lastName nvarchar(50), 
+       age int, dateOfBirth datetime2)
