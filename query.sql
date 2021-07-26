63815262121e9d2b67c4426b7fe35078eb733cef
pulipativedanth <80522589+pulipativedanth@users.noreply.github.com>
2021-07-26 14:24:01 +0530

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,4 +1,4 @@
 SELECT *
-FROM OPENJSON(@json)
+FROM OPENROWSET(BULK'./user1.json',SINGLE_BLOB) JSON
      WITH (id int, firstName nvarchar(50), lastName nvarchar(50),
            age int, dateOfBirth datetime2)
