a2efc4dece1c159a8dbee8503e86b844b2979b4f
pulipativedanth <80522589+pulipativedanth@users.noreply.github.com>
2021-07-26 13:46:04 +0530

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,6 +1,20 @@
-declare  [customer] 
+declare  [customer] varchar(max)
 	 
 select [customer] =
 	BulkColumn
 	from openrowset(bulk './user1.json', single_clob) JSON
 
+Insert into customer
+select *
+from openjson (customer)
+with (
+	C_CUSTKEY int  '$.C_CUSTKEY',
+	C_NAME varchar(25) '$.C_NAME',
+	C_ADDRESS varchar(40) '$.C_ADDRESS',
+	C_NATIONKEY int '$.C_NATIONKEY ',
+	C_PHONE char(15) '$.C_PHONE',
+	C_ACCTBAL decimal(15, 2) '$.C_ACCTBAL',
+	C_MKTSEGMENT char(10) '$.C_MKTSEGMENT',
+	C_COMMENT varchar(117) '$.C_COMMENT'
+	)
+End
