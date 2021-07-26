98be405e4b250830e303eaad09bde4c6f0a07c34
pulipativedanth <80522589+pulipativedanth@users.noreply.github.com>
2021-07-26 14:20:05 +0530

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,19 +1,4 @@
-select 
-	cast(bulkcolumn as nvarchar(max)) as jsondata 
-from 
-	openrowset(bulk './user1.json',  single_clob) as azureblob
-
-Insert into [dbo].[customer]
-select *
-from openjson ([dbo].[customer], '$.customer')
-with (
-	C_CUSTKEY int  '$.C_CUSTKEY',
-	C_NAME varchar(25) '$.C_NAME',
-	C_ADDRESS varchar(40) '$.C_ADDRESS',
-	C_NATIONKEY int '$.C_NATIONKEY ',
-	C_PHONE char(15) '$.C_PHONE',
-	C_ACCTBAL decimal(15, 2) '$.C_ACCTBAL',
-	C_MKTSEGMENT char(10) '$.C_MKTSEGMENT',
-	C_COMMENT varchar(117) '$.C_COMMENT'
-	)
-go
+SELECT *
+FROM OPENJSON(@json)
+     WITH (id int, firstName nvarchar(50), lastName nvarchar(50),
+           age int, dateOfBirth datetime2)
