ab5c9e796a56249a6c422db2189f96d188d00f8f
pulipativedanth <80522589+pulipativedanth@users.noreply.github.com>
2021-07-26 21:36:49 +0530

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,10 +1,1 @@
-DECLARE @JSON VARCHAR(MAX)
-SELECT @JSON = BulkColumn
-FROM OPENROWSET(BULK'C:\Users\pulipati.vedanth\Downloads\generated.json',SINGLE_BLOB) As J
-SELECT Name,Gender,Company,Email into EmployeeDetails
-FROM OPENJSON(@JSON,'$.data')
-WITH (name varchar(100) ,
-       Gender varchar(100) ,
-	 Company varchar(100) , 
-       Email Varchar (100)
-	 )
+INSERT INTO dbo.[commitdetails ](Repository,CommittedID,Branch,CommitDate,CommitedAuthor) values($(ColValue1),$(ColValue3),$(ColValue2),$(ColValue4),$(ColValue5))
