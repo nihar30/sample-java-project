38d997a8fc915489d68b97c64240d624fcedbc9d
pulipativedanth <80522589+pulipativedanth@users.noreply.github.com>
2021-07-28 12:06:39 +0530

    JSON Export

---
+++ InsertQuery.sql
@@ -0,0 +1,21 @@
+Drop table books;
+Create table books( Id NCHAR(14), [Name] NCHAR(30), Author NCHAR(30) )
+SELECT id,[name],author FROM OPENROWSET(
+    BULK  'books.json',
+    DATA_SOURCE = 'MyAzureBlobStorage22',
+     SINGLE_CLOB
+     ) AS DataFile
+ CROSS APPLY OPENJSON(BulkColumn)
+ WITH (id NCHAR(14),
+ [name] NCHAR(30),
+ author NCHAR(30)) as book
+INSERT INTO books with (TABLOCK) (id,[name],author)
+ SELECT id,[name],author FROM OPENROWSET(
+    BULK  'books.json',
+    DATA_SOURCE = 'MyAzureBlobStorage22',
+     SINGLE_CLOB
+     ) AS DataFile
+ CROSS APPLY OPENJSON(BulkColumn)
+ WITH (id NCHAR(14),
+ [name] NCHAR(30),
+ author NCHAR(30)) as book
